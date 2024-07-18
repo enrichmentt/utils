2f2f3fc6408721284947252ba9c537f73b0e19e1
Vladimir Pekshev <enrichment.off@gmail.com>
2024-07-18 19:48:34 +0300

    New func ContainsInt

--- utils.go
+++ utils.go
@@ -3,9 +3,18 @@ package utils
 func Contains(s []string, str string) bool {
 	for _, v := range s {
 		if v == str {
-
 			return true
 		}
 	}
+	return false
+}
+
+func ContainsInt(a []int, x int) bool {
+	for _, v := range a {
+		if v == x {
+			return true
+		}
+	}
+
 	return false
 }
