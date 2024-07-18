2d31cf7a42bf6296b8d991f25dc8d16dcdccb1f6
Vladimir Pekshev <enrichment.off@gmail.com>
2024-07-18 20:08:18 +0300

    New version

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/enrichmentt/utils
+module github.com/enrichmentt/utils/v2
 
 go 1.22.5
--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func Contains(s []string, str string) bool {
+func InSlice(s []string, str string) bool {
 	for _, v := range s {
 		if v == str {
 			return true
